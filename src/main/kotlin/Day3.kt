46b102ef0a02f87c4586af50c54f896e1531d29a
Kamil Gurgul <kamgurgul@gmail.com>
2021-12-03 17:38:50 +0100

    Day 3 -> not working for part 2

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -12,23 +12,30 @@ fun flipBinaryString(input: String): String {
     return builder.toString()
 }
 
-fun main() {
-    val input = File("./src/main/resources/day_3/input.txt")
+fun Array<CharArray>.rotate(): Array<CharArray> {
+    val rows = size
+    val columns = first().size
+    val rotated = Array(columns) { CharArray(rows) { '0' } }
+    for (i in 0 until columns) {
+        for (j in rows - 1 downTo 0) {
+            val newX = rows - 1 - j
+            rotated[i][newX] = get(j)[i]
+        }
+    }
+    return rotated
+}
+
+fun loadDataAsMatrix(): Array<CharArray> {
+    return File("./src/main/resources/day_3/test_input.txt")
         .readLines()
         .filterNot { it.isEmpty() }
         .map { it.toCharArray() }
         .toTypedArray()
+}
 
-    val recordsCount = input.size
-    val recordSize = input[0].size
-
-    val rotatedInput = Array(recordSize) { CharArray(recordsCount) { '0' } }
-    for (i in 0 until recordSize) {
-        for (j in recordsCount - 1 downTo 0) {
-            val newX = recordsCount - 1 - j
-            rotatedInput[i][newX] = input[j][i]
-        }
-    }
+fun part1(): Int {
+    val input = loadDataAsMatrix()
+    val rotatedInput = input.rotate()
 
     val binaryGamma = rotatedInput
         .map {
@@ -46,9 +53,43 @@ fun main() {
     val gamma = binaryGamma.toInt(2)
     val epsilon = binaryEpsilon.toInt(2)
 
-    println(gamma)
-    println(epsilon)
+    return gamma * epsilon
+}
 
-    val result = gamma * epsilon
-    println(result)
+fun findOxygenRow(input: Array<CharArray>, rotatedInput: Array<CharArray>): String {
+    val mutableInput = input.toMutableList()
+    val mutableRotatedInput = rotatedInput.toMutableList()
+    for ((i, row) in mutableRotatedInput.withIndex()) {
+        val ones = row.count { char -> char == '1' }
+        val zeros = row.size - ones
+        if (ones >= zeros) {
+            mutableInput.removeAll { it[i] == '0' }
+        } else {
+            mutableInput.removeAll { it[i] == '1' }
+        }
+
+        /*mutableRotatedInput.clear()
+        mutableInput.addAll(mutableInput.toTypedArray().rotate())*/
+
+        if (mutableInput.size == 1) {
+            break
+        }
+    }
+    return mutableInput.first().joinToString("")
+}
+
+fun part2(): Int {
+    val input = loadDataAsMatrix()
+    val rotatedInput = input.rotate()
+
+    val binaryOxygen = findOxygenRow(input, rotatedInput)
+
+    println(binaryOxygen)
+
+    return 0
+}
+
+fun main() {
+    println(part1())
+    println(part2())
 }
