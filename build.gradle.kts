d7951c4a5106fb59aa5fa1e8501829d4e62cf23c
Kamil Gurgul <kamgurgul@gmail.com>
2021-12-06 20:35:36 +0100

    Day 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.6.0"
     application
 }
 
@@ -25,5 +25,5 @@ tasks.withType<KotlinCompile> {
 }
 
 application {
-    mainClass.set("MainKt")
+    mainClass.set("Day1.kt")
 }
--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.abs
 
 data class Coords(
     val x: Int,
@@ -10,24 +11,114 @@ data class VentLine(
     val to: Coords
 ) {
 
-    fun isVerticalOrHorizontalLine() =
-        from.x == to.x || from.y == to.y
+    fun isVerticalLine() = from.y == to.y
+
+    fun isHorizontalLine() = from.x == to.x
+
+    fun toList() = buildList {
+        add(from.x)
+        add(from.y)
+        add(to.x)
+        add(to.y)
+    }
+
+    companion object {
+
+        fun parse(input: String): VentLine {
+            val numbers = input.split(",", " -> ")
+            val from = Coords(numbers[0].toInt(), numbers[1].toInt())
+            val to = Coords(numbers[2].toInt(), numbers[3].toInt())
+            return VentLine(from, to)
+        }
+    }
 }
 
 class VentsMap(
-    val ventLines: List<VentLine>,
-    val map: List<List<Int>>
+    private val ventLines: List<VentLine>
 ) {
 
+    private val map: Array<IntArray>
+
+    init {
+        val mapSize = calculateMapSize()
+        map = Array(mapSize) { IntArray(mapSize) { 0 } }
+    }
+
+    fun draw(withDiagonal: Boolean = false) {
+        ventLines.forEach {
+            if (it.isHorizontalLine()) {
+                if (it.from.y >= it.to.y) {
+                    for (y in it.from.y downTo it.to.y) {
+                        markSpot(it.from.x, y)
+                    }
+                } else {
+                    for (y in it.from.y..it.to.y) {
+                        markSpot(it.from.x, y)
+                    }
+                }
+            } else if (it.isVerticalLine()) {
+                if (it.from.x >= it.to.x) {
+                    for (x in it.from.x downTo it.to.x) {
+                        markSpot(x, it.from.y)
+                    }
+                } else {
+                    for (x in it.from.x..it.to.x) {
+                        markSpot(x, it.from.y)
+                    }
+                }
+            } else if (withDiagonal) {
+                val leftTopDir = it.from.x > it.to.x && it.from.y > it.to.y
+                val rightTopDir = it.from.x < it.to.x && it.from.y > it.to.y
+                val leftBottomDir = it.from.x > it.to.x && it.from.y < it.to.y
+                val length = abs(it.from.x - it.to.x)
+                for (counter in 0..length) {
+                    when {
+                        leftTopDir -> markSpot(it.from.x - counter, it.from.y - counter)
+                        rightTopDir -> markSpot(it.from.x + counter, it.from.y - counter)
+                        leftBottomDir -> markSpot(it.from.x - counter, it.from.y + counter)
+                        else -> markSpot(it.from.x + counter, it.from.y + counter)
+                    }
+                }
+            }
+        }
+    }
+
+    fun getDangerSpotsAmount() = map.sumOf { row -> row.count { it > 1 } }
+
+    private fun markSpot(x: Int, y: Int) {
+        map[y][x] = map[y][x] + 1
+    }
+
+    private fun calculateMapSize(): Int {
+        val allCoords = buildList { ventLines.forEach { addAll(it.toList()) } }
+        return allCoords.maxOf { it } + 1
+    }
 }
 
 fun main() {
-    fun part1(): String {
-        val input = File("./src/main/resources/day_4/input.txt")
+    fun part1(): Int {
+        val input = File("./src/main/resources/day_5/input.txt")
             .readLines()
             .filterNot { it.isEmpty() }
+            .map { VentLine.parse(it) }
 
-        return ""
+        val ventsMap = VentsMap(input)
+        ventsMap.draw()
+
+        return ventsMap.getDangerSpotsAmount()
+    }
+
+    fun part2(): Int {
+        val input = File("./src/main/resources/day_5/input.txt")
+            .readLines()
+            .filterNot { it.isEmpty() }
+            .map { VentLine.parse(it) }
+
+        val ventsMap = VentsMap(input)
+        ventsMap.draw(withDiagonal = true)
+
+        return ventsMap.getDangerSpotsAmount()
     }
     println(part1())
+    println(part2())
 }
