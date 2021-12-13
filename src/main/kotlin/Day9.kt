5900993cf710355fe65386676c3524ac4539ca07
Kamil Gurgul <kamgurgul@gmail.com>
2021-12-13 20:18:09 +0100

    Add day 9 part 2

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -13,20 +13,20 @@ fun main() {
         private val input: Array<IntArray>
     ) {
 
-        val localBasinsList = mutableListOf<Basin>()
+        private val localBasinsList = mutableListOf<Basin>()
+        private val visitedLocations = mutableSetOf<Pair<Int, Int>>()
 
         fun calculateBasins() {
-            localBasinsList.clear()
             val width = input.first().size
             val height = input.size
             for (i in 0 until height) {
                 for (j in 0 until width) {
                     val left = input.getOrNull(i)?.getOrNull(j - 1)
-                    val top = input.getOrNull(i - 1)?.getOrNull(j)
+                    val up = input.getOrNull(i - 1)?.getOrNull(j)
                     val right = input.getOrNull(i)?.getOrNull(j + 1)
-                    val bottom = input.getOrNull(i + 1)?.getOrNull(j)
+                    val down = input.getOrNull(i + 1)?.getOrNull(j)
                     val center = input[i][j]
-                    val neighbours = listOfNotNull(left, top, right, bottom)
+                    val neighbours = listOfNotNull(left, up, right, down)
                     var isLocalMin = true
                     neighbours.forEach {
                         if (center >= it) {
@@ -41,6 +41,42 @@ fun main() {
         }
 
         fun getRiskSum() = localBasinsList.sumOf { it.risk }
+
+        fun getThreeBiggestBasins(): Int {
+            return localBasinsList
+                .map {
+                    visitedLocations.clear()
+                    getBasinSizeRec(it.x, it.y)
+                    visitedLocations.size
+                }
+                .sortedDescending()
+                .take(3)
+                .reduce { acc, area -> acc * area }
+        }
+
+        private fun getBasinSizeRec(i: Int, j: Int) {
+            visitedLocations.add(i to j)
+            val canGoLeft = (i to j - 1) !in visitedLocations
+                    && input.getOrNull(i)?.getOrNull(j - 1)?.let { it < 9 } ?: false
+            val canGoUp = (i - 1 to j) !in visitedLocations
+                    && input.getOrNull(i - 1)?.getOrNull(j)?.let { it < 9 } ?: false
+            val canGoRight = (i to j + 1) !in visitedLocations
+                    && input.getOrNull(i)?.getOrNull(j + 1)?.let { it < 9 } ?: false
+            val canGoDown = (i + 1 to j) !in visitedLocations
+                    && input.getOrNull(i + 1)?.getOrNull(j)?.let { it < 9 } ?: false
+            if (canGoLeft) {
+                getBasinSizeRec(i, j - 1)
+            }
+            if (canGoUp) {
+                getBasinSizeRec(i - 1, j)
+            }
+            if (canGoRight) {
+                getBasinSizeRec(i, j + 1)
+            }
+            if (canGoDown) {
+                getBasinSizeRec(i + 1, j)
+            }
+        }
     }
 
     fun part1(): Int {
@@ -58,7 +94,13 @@ fun main() {
     fun part2(): Int {
         val input = File("./src/main/resources/day_9/input.txt")
             .readLines()
-        return 0
+            .filter { it.isNotEmpty() }
+            .map { line -> line.map { it.digitToInt() } }
+            .map { it.toIntArray() }
+            .toTypedArray()
+        val map = Map(input)
+        map.calculateBasins()
+        return map.getThreeBiggestBasins()
     }
     println(part1())
     println(part2())
